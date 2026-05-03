40729aa6c43775e0446d8725c287ec39ec87891a
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-05-02 23:45:14 -0300

    Fix: Adicionando controller FlashController

---
+++ app/Controllers/FlashController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    public function flash(): void
+        {
+            $this->json([]);
+        }
+}
