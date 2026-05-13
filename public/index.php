83095f61b89cdf549453aff99b42fc0e6f143cf8
FelipeFadel <dasilvafadelfelipe@gmail.com>
2026-05-12 23:39:28 -0300

    aceita requisições de portas externas

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
 require __DIR__ . '/../config/bootstrap.php';
